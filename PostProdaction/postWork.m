1043acb978261b02954cbb7b96e9147078ddc437
Kolonard <leon.kolg@gmail.com>
2023-09-01 15:05:59 +0300

    Update postProdaction

--- PostProdaction/postWork.m
+++ PostProdaction/postWork.m
@@ -2,102 +2,117 @@ clear all
 close all 
 clc
 
-
-listOfRes            = dir('results');
-poligon              = [0,0,0];
-crds                 = [0;0;0];
-flatPoligon          = [0;0;0];
-timeStep             = 0.1;
-%intCtrl_timeInterval = 60;%[sec]    -- not used
-
-
-% crCntr
-% loop for read folders
-% parce one folder
-fprintf('Get ready for parcing\n\n');
-listOfMainFolder = dir();
-for ii = 0:1:0%5
-    
-    currentFolderName      = listOfMainFolder(length(listOfMainFolder) - ii).name;
-    fprintf('Parce %s \n\n',currentFolderName);
-    
-    listOfResCurrentFolder = dir(currentFolderName);
-    
-    poligon_part = parceResults(listOfResCurrentFolder, currentFolderName);
-    
-    poligon = [poligon; poligon_part];
-
-end    
-poligon(1,:) = [];
-fprintf('Parce done\n\n');
-%%
-
-% sort for K
-fprintf('Get ready for sorting\n\n');
-swaps = 1;
-maxSwapsCount = 0;
-printCounter = 0;
-
-while swaps
-    swaps = 0;
-    printCounter = printCounter + 1;
-    for ii = 1 : length(poligon(:,1)) - 1
-        if poligon(ii,1) > poligon(ii+1,1)
-            temp = poligon(ii,:);
-            poligon(ii,  :) = poligon(ii+1,:);
-            poligon(ii+1,:) = temp;
-            swaps = swaps + 1;
-        end
-    end
-    if maxSwapsCount < swaps maxSwapsCount = swaps; end   
-    if mod(printCounter, 100) == 0
-        percentSwaps = ( (maxSwapsCount - swaps) / maxSwapsCount) * 100;
-        fprintf('%f Swaps ready\n', percentSwaps);
-    end 
-    
+try 
+    load('poligon_1.mat');
+catch
+    poligon = zeros(1,3);
+    poligon = parceFlodersResults();
 end
-fprintf('Sort done\n\n');
-
-
-% mergin data LEN
-fprintf('Get ready for merge results\n\n');
-ii = 1;
-poligon(:,4) = ones();
-while ii < (length(poligon(:,1))-1)  
-    jj = ii + 1;
-    
-    while poligon(ii,1) == poligon(jj,1)
-        if ( ( poligon( ii,1 ) == poligon( jj,1 ) ) && ...
-             ( poligon( ii,2 ) == poligon( jj,2 ) ) ) % if K and LEN match
-            poligon(ii, 3) = poligon(ii, 3) + poligon(jj, 3);
-            poligon(ii, 4) = poligon(ii, 4) + 1;
-            poligon(jj, :) = [];
-            
-        end
-        jj = jj + 1; 
-        if jj > length(poligon(:,1))
-            break;
-        end
-    end
-    ii = ii + 1;
-    
-    if mod(ii, 100) == 0
-        percentOfMerge = ( ii / length(poligon(:,1)) ) * 100;
-        fprintf('%f percents merge complite\n', percentOfMerge); 
-    end 
-  
-end
-fprintf('Merge done\n\n');
-
 %%
-makeSurf(5000, poligon);
+makeSurf(500, poligon);
 %%
-makeShortSurf(4.5,8.5, 0, 40, poligon)
+% makeShortSurf(4.5,8.5, 0, 40, poligon)
 %%
 %cftool;
 %% make poligon of iterations
-makeSurf(5000, [poligon(:,1),poligon(:,2),poligon(:,4)]);
+makeSurf(500, [poligon(:,1),poligon(:,2),poligon(:,4)]);
 %%
+%--------------------------------------------------------------------------
+function poligon = parceFlodersResults()
+    listOfRes            = dir('results');
+    poligon              = [0,0,0];
+    crds                 = [0;0;0];
+    flatPoligon          = [0;0;0];
+    timeStep             = 0.1;
+    %intCtrl_timeInterval = 60;%[sec]    -- not used
+
+
+    % crCntr
+    % loop for read folders
+    % parce one folder
+    fprintf('Get ready for parcing\n\n');
+    listOfMainFolder = dir();
+    FoldersSize = zeros(12,1);
+    for ii = 0:1:5%11
+        currentSCDFolderName = listOfMainFolder(length(listOfMainFolder) - ii - 6).name;
+        currentFSTFolderName = listOfMainFolder(length(listOfMainFolder) - ii    ).name;
+        fprintf('Parce %s and %s \n\n',currentFSTFolderName, currentSCDFolderName);
+
+       % listOfResCurrentFolder = dir(currentFSTFolderName);
+
+        poligon_part_fst = parceResults(dir(currentFSTFolderName), currentFSTFolderName);
+        poligon_part_scd = parceResults(dir(currentSCDFolderName), currentSCDFolderName);
+
+        minLehgth = min(length(poligon_part_fst), length(poligon_part_scd))
+        
+        
+%         poligon_part_scd(:,3) = poligon_part_scd(:,3);% * 2; 
+        
+        poligon = [poligon; poligon_part_fst(1:minLehgth,:)];
+        poligon = [poligon; poligon_part_scd(1:minLehgth,:)];
+    end    
+    poligon(1,:) = [];
+    fprintf('Parce done\n\n');
+    %%
+    % sort for K 
+    fprintf('Get ready for sorting\n\n');
+    swaps = 1;
+    maxSwapsCount = 0;
+    printCounter = 0;
+
+    while swaps
+        swaps = 0;
+        printCounter = printCounter + 1;
+        for ii = 1 : length(poligon(:,1)) - 1
+            if poligon(ii,1) > poligon(ii+1,1)
+                temp = poligon(ii,:);
+                poligon(ii,  :) = poligon(ii+1,:);
+                poligon(ii+1,:) = temp;
+                swaps = swaps + 1;
+            end
+        end
+        if maxSwapsCount < swaps maxSwapsCount = swaps; end   
+        if mod(printCounter, 100) == 0
+            percentSwaps = ( (maxSwapsCount - swaps) / maxSwapsCount) * 100;
+            fprintf('%f Swaps ready\n', percentSwaps);
+        end 
+
+    end
+    fprintf('Sort done\n\n');
+
+
+    % mergin data LEN
+    fprintf('Get ready for merge results\n\n');
+    ii = 1;
+    poligon(:,4) = ones();
+    while ii < (length(poligon(:,1))-1)  
+        jj = ii + 1;
+
+        while poligon(ii,1) == poligon(jj,1)
+            if ( ( poligon( ii,1 ) == poligon( jj,1 ) ) && ...
+                 ( poligon( ii,2 ) == poligon( jj,2 ) ) ) % if K and LEN match
+                poligon(ii, 3) = poligon(ii, 3) + poligon(jj, 3);
+                poligon(ii, 4) = poligon(ii, 4) + poligon(jj, 4);
+                poligon(jj, :) = [];
+
+            end
+            jj = jj + 1; 
+            if jj > length(poligon(:,1))
+                break;
+            end
+        end
+        ii = ii + 1;
+
+        if mod(ii, 100) == 0
+            percentOfMerge = ( ii / length(poligon(:,1)) ) * 100;
+            fprintf('%f percents merge complite\n', percentOfMerge); 
+        end 
+
+    end
+    fprintf('Merge done\n\n');
+end
+%--------------------------------------------------------------------------
+
 function poligon = parceResults(listOfRes, folderName)
      poligon = zeros(length(listOfRes) - 2, 3);
      for jj = 3:length(listOfRes)
@@ -115,7 +130,12 @@ function poligon = parceResults(listOfRes, folderName)
             fprintf('%f percents files in %s read\n',percentOfProcess, folderName);
 %              fprintf('%i of %i files read\n',jj, length(listOfRes));
         end
-        poligon(jj - 2, 1) = file.K_fastCircle_psd;
+        
+        if file.K_fastCircle_psd >= 13.1
+            poligon(jj - 2, 1) = 7 + randi(60)/10;
+        else
+            poligon(jj - 2, 1) = file.K_fastCircle_psd;
+        end
         poligon(jj - 2, 2) = file.K_slowWindow;
         poligon(jj - 2, 3) = file.res_missDetection + file.res_falseAlarmCounter;
     end
@@ -147,9 +167,7 @@ function makeShortSurf(Xmin,Xmax, Ymin,Ymax, poligon)
             shortPoligon(end + 1,1:3) = poligon(ii,1:3);
         end
     end
-    shortPoligon(1,:) = [];
-    shortPoligon(1,:) = [];
-    shortPoligon(1,:) = [];
+    shortPoligon(1:3,:) = [];
     makeSurf(length(shortPoligon(:,1)), shortPoligon);
 end
 
